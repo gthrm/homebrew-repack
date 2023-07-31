e761978b939f2a30aabbd2e2fe953c3a0c3676f1
cdROma <38502733+gthrm@users.noreply.github.com>
2023-07-31 10:21:42 +0200

    fix: sha256 fixed

--- repack.rb
+++ repack.rb
@@ -2,7 +2,7 @@ class Repack < Formula
   desc "A shell script for repackaging the node_modules in any JavaScript project, providing the option to use either npm or yarn."
   homepage "https://github.com/gthrm/homebrew-repack"
   url "https://github.com/gthrm/homebrew-repack/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "f91b9934c25e25dbbde2c0b6ecc1b679f64376116105237ca47b79e4e815eb68"
+  sha256 "d41c84fdb6a4682fe5c9543f45f0f8729977b630a31bcbb43803718a17eafc64"
 
   def install
     bin.install "repack.sh" => "repack"
