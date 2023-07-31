00d691953fad888b5792dc26493ef5e5861ca2aa
cdROma <38502733+gthrm@users.noreply.github.com>
2023-07-31 10:45:32 +0200

    Update repack.rb

--- repack.rb
+++ repack.rb
@@ -1,7 +1,7 @@
 class Repack < Formula
   desc "A shell script for repackaging the node_modules in any JavaScript project, providing the option to use either npm or yarn."
   homepage "https://github.com/gthrm/homebrew-repack"
-  url "https://github.com/gthrm/homebrew-repack/archive/refs/tags/v1.0.3.tar.gz"
+  url "https://github.com/gthrm/homebrew-repack/archive/refs/tags/v1.0.4.tar.gz"
   sha256 "d41c84fdb6a4682fe5c9543f45f0f8729977b630a31bcbb43803718a17eafc64"
 
   def install
